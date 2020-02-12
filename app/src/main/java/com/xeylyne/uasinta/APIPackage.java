db8c7b02b87c9edde75719a76fa1d56958a388ed
rzldev <amrizaldev00@gmail.com>
2020-02-12 07:50:59 +0700

    Respond Request

--- app/src/main/java/com/xeylyne/uasinta/APIPackage.java
+++ app/src/main/java/com/xeylyne/uasinta/APIPackage.java
@@ -1,11 +1,23 @@
 package com.xeylyne.uasinta;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface APIPackage {
 
     @GET("pl_getPlace.php")
     Call<ReqAPI> request();
+
+    @FormUrlEncoded
+    @POST("clustering")
+    Call<ClusteringRespond> CLUSTERING_RESPOND_CALL(@Field("region") String region,
+                                                    @Field("item") String item);
+
+    @FormUrlEncoded
+    @POST("estimation")
+    Call<EstimationRespond> ESTIMATION_RESPOND_CALL(@Field("item") String item);
 
 }
